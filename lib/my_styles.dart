5ddf0eda3c6cdc395185c467ab2290ab89dac5b4
Никита <ilyinnikita90@mail.ru>
2024-02-28 01:19:53 +0300

    FINAL

---
+++ lib/my_styles.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+const TextStyle xoStyle = TextStyle(fontWeight: FontWeight.bold,fontSize:50.0, color: Colors.white );
+
+const TextStyle txtStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.white);
